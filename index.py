9184607af5d8a3c9467089b0defa1c6ef7c66ece
Olmira <olmbel@gmail.com>
2023-11-23 17:35:29 +0100

    Update index.py

--- index.py
+++ index.py
@@ -27,3 +27,34 @@ def game(nom_pays='France'):
 #town_name = st.text_input("Rntrer le nom de votre ville:")
 country_name = st.text_input("Rentrer le nom de votre pays (en anglais):")
 st.write(game(country_name))
+
+
+# Simple pygame program
+
+# Import and initialize the pygame library
+import pygame
+pygame.init()
+
+# Set up the drawing window
+screen = pygame.display.set_mode([500, 500])
+
+# Run until the user asks to quit
+running = True
+while running:
+
+    # Did the user click the window close button?
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Fill the background with white
+    screen.fill((255, 255, 255))
+
+    # Draw a solid blue circle in the center
+    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+
+    # Flip the display
+    pygame.display.flip()
+
+# Done! Time to quit.
+pygame.quit()
